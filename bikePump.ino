53e2ffc7097b4977157bbbb59e50bf347d0fb6d5
jkasari <jkasari@outlook.com>
2022-10-24 17:03:47 -0700

    Set up ino file

---
+++ bikePump.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
+#include <LiquidCrystal.h>
+
+#define LCD_RS 3
+#define LCD_E 5
+#define LCD_D4 2
+#define LCD_D5 4
+#define LCD_D6 8
+#define LCD_D7 9
+#define BUTTON1 A3
+#define BUTTON2 10
+#define BUTTON3 11
+#define BUTTON4 A2
+#define BUTTON5 A1
+#define BUTTON6 12
+#define PSI_CONSTANT 0.015625
+#define GATE_ONE 6
+#define GATE_TWO 7
+
+void setup() {
+
+}
+
+void loop() {
+
+}
