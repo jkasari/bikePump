44c17db7439b852a41e34e70ab477c78f833d963
jkasari <jkasari@outlook.com>
2022-10-24 18:41:41 -0700

    Added the sensor to main

--- bikePump.ino
+++ bikePump.ino
@@ -6,15 +6,24 @@
 
 #define PSI_CONSTANT 0.015625
 
+Adafruit_ADS1115 ADS;
 DisplayControl Display;
 MainController Controller;
 
 void setup() {
     Serial.begin(115200);
+    ADS.begin();
+    ADS.setGain(GAIN_SIXTEEN);
 }
 
 void loop() {
+    float currentPSI = getReading();
     Controller.testingFunction();
-    Display.displayNumber(66.6);
+    Display.displayNumber(currentPSI);
     delay(1000);
+}
+
+
+float getReading() {
+    return ADS.readADC_Differential_0_1() * PSI_CONSTANT + .40;
 }
